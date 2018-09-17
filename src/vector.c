c06b11fa67ec5a2e4e400c3b17bf51a85610d22d
neonphog <neonphog@gmail.com>
2018-09-17 15:04:29 -0600

    valgrind && indent

--- src/vector.c
+++ src/vector.c
@@ -14,26 +14,29 @@ cmo_vector *cmo_vector_alloc(_cmo_ctx *_ctx) {
 }
 
 void cmo_vector_realloc(cmo_vector *v, int32_t size) {
-   int32_t i, shift;
-   void * * new_data;
+  int32_t i, shift;
+  void * * new_data;
 
-   shift = (size - v->size) / 2;
+  shift = (size - v->size) / 2;
 
-   new_data = (void * *) v->_ctx->alloc(
-       v->_ctx->alloc_usr, sizeof(void *) * size);
+  new_data = (void * *) v->_ctx->alloc(
+      v->_ctx->alloc_usr, sizeof(void *) * size);
 
-   for (i = v->start_idx; i < v->end_idx; ++i) {
-      new_data[i + shift] = v->data[i];
-   }
+  for (i = v->start_idx; i < v->end_idx; ++i) {
+    new_data[i + shift] = v->data[i];
+  }
 
-   v->_ctx->free(v->_ctx->free_usr, v->data);
+  if (v->data != 0) {
+    v->_ctx->free(v->_ctx->free_usr, v->data);
+  }
 
-   v->data = new_data;
-   v->size = size;
-   v->start_idx += shift;
-   v->end_idx += shift;
+  v->data = new_data;
+  v->size = size;
+  v->start_idx += shift;
+  v->end_idx += shift;
 }
 
 void cmo_vector_free(cmo_vector *v) {
+  v->_ctx->free(v->_ctx->free_usr, v->data);
   v->_ctx->free(v->_ctx->free_usr, v);
 }
